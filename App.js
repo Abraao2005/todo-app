1fb82c20d31c417cb1142be5bdee9792f20b2426
AlunoTI <AlunoTI@estacio.acad>
2025-09-15 20:20:55 -0300

    Inicializando o projeto

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import styled from 'styled-components/native';
 export default function App() {
+
+  
+
+  
+  // const Button = styled.button`
+  // background-color:black;
+  // color:red;`
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
