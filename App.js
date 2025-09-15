78bbe0d8cb7589906d6460c3371a0b20cb196534
AlunoTI <AlunoTI@estacio.acad>
2025-09-15 20:41:29 -0300

    Dando continuidade

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native';
+import { useFonts } from 'expo-font';
 export default function App() {
+  const [loaded] = useFonts({
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf')
+  });
 
-  
 
-  
-  // const Button = styled.button`
-  // background-color:black;
-  // color:red;`
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontFamily: "GeneralSans-400"
+      }}>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontFamily: "GeneralSans-600"
+      }}>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontFamily: "GeneralSans-700"
+      }}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
